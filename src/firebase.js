14d9908972d587c06c947e28da1b74a0e3332f13
Athena <athena.partch@gmail.com>
2021-05-15 08:13:00 -0700

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAYP2xQdI_nGYZ7IrE3ei3qOCgooieiSfw",
+    authDomain: "clone-d1d19.firebaseapp.com",
+    projectId: "clone-d1d19",
+    storageBucket: "clone-d1d19.appspot.com",
+    messagingSenderId: "250156602742",
+    appId: "1:250156602742:web:b392aea9fad5a5d1abd657",
+    measurementId: "G-16PJGWM64V"
+  };
